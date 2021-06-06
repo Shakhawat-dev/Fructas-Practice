0b4c43a7783a598164b2c0fe9f73ceaa63a74080
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2021-06-07 00:18:47 +0600

    Initial Commit

---
+++ Shared/Fructas_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fructas_PracticeApp.swift
+//  Shared
+//
+//  Created by Shakhawat Hossain Shahin on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Fructas_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
